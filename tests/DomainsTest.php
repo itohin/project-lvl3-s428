d3bb72956d6a9c5218014b890b8dde52bf6b75a5
itohin <itoxa@MacBook-Pro.local>
2019-03-27 01:15:21 +0300

    tests

--- tests/DomainsTest.php
+++ tests/DomainsTest.php
@@ -15,4 +15,22 @@ class DomainsTest extends TestCase
         $response = $this->get('/');
         $response->assertResponseStatus(200);
     }
+
+    public function testStoreDomain()
+    {
+        $url = 'http://domain.com';
+        $domain = Domain::create(['name' => $url]);
+
+        $this->post('/domains', $domain->toArray());
+        $this->seeInDatabase('domains', ['name' => $url]);
+    }
+
+    public function testShowDomain()
+    {
+        $url = 'http://domain.com';
+        $domain = Domain::create(['name' => $url]);
+
+        $response = $this->get("/domains/{$domain->id}");
+        $response->assertResponseStatus(200);
+    }
 }
